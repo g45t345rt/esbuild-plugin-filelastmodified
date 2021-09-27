1c9c5819688218899525d49c89f0364a6b53a746
g45t345rt <fred10.cha@gmail.com>
2021-09-27 19:00:11 -0400

    remove onResolve

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,15 @@
-import { Plugin, OnResolveArgs, OnLoadArgs, OnResolveResult, OnLoadResult } from 'esbuild'
+import { Plugin, OnLoadArgs, OnLoadResult } from 'esbuild'
 import fs from 'fs'
-import path from 'path'
-
-const LOAD_NAMESPACE = 'load_filelastmodified_namespace'
 
 interface PluginOptions {
   identifier?: string
 }
 
-const onResolve = (identifier) => async (args: OnResolveArgs): Promise<OnResolveResult> => {
+const onLoad = (identifier) => async (args: OnLoadArgs): Promise<OnLoadResult> => {
   const find = new RegExp(identifier, 'g')
-  if (!args.path.startsWith('.')) return
   const source = (await fs.promises.readFile(args.path)).toString('utf-8')
 
   if (!source.match(find)) return
-
-  return {
-    path: args.path,
-    namespace: LOAD_NAMESPACE,
-    pluginData: { source, find }
-  }
-}
-
-const onLoad = (args: OnLoadArgs): OnLoadResult => {
-  const { pluginData } = args
-  const { source, find } = pluginData
-
   const stats = fs.statSync(args.path) // get file information -> last time it was modified
   const contents = source.replace(find, stats.mtimeMs.toString()) // replace the identifier with file last modified
   return { contents }
@@ -37,8 +21,7 @@ const plugin = (options?: PluginOptions): Plugin => {
   return {
     name: 'esbuild-plugin-filelastmodified',
     setup(build) {
-      build.onResolve({ filter: /.*/ }, onResolve(identifier))
-      build.onLoad({ filter: /.*/, namespace: LOAD_NAMESPACE }, onLoad)
+      build.onLoad({ filter: /.*/ }, onLoad(identifier))
     }
   }
 }
